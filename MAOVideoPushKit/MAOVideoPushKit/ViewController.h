5d460099b47697d3a6b78b1167a922cf515d8bad
supermao <625257555@qq.com>
2016-03-30 22:39:17 +0800

    first blood

---
+++ MAOVideoPushKit/MAOVideoPushKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MAOVideoPushKit
+//
+//  Created by Mao on 16/3/30.
+//  Copyright © 2016年 AppGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
